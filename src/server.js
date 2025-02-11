585d107f8729bab05958403bf853757b8ea4aec3
Robson <robindiegoa@gmail.com>
2025-02-11 16:50:45 -0300

    :sparkles: feat: headers and statefull concepts

--- src/server.js
+++ src/server.js
@@ -2,16 +2,20 @@ import http from 'http'
 
 const port = 8080
 
+// Statefull data
+const users = []
+
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
-    if (method === 'GET' && url === '/test') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        return res.end('Test route')
+    if (method === 'GET' && url === '/users') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        return res.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/user') {
-        res.writeHead(201, { 'Content-Type': 'text/plain' })
+        users.push({ id: users.length + 1, name: 'John Doe' })
+        res.writeHead(201, { 'Content-Type': 'application/json' })
         return res.end('User created')
     }
 
